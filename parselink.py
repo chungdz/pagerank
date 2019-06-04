fe010ee00fad53c19ec0586cba9fb46667c59d70
alienware m15 <1033718596@qq.com>
2019-06-04 18:11:52 +0800

    two type of files

--- parselink.py
+++ parselink.py
@@ -29,5 +29,31 @@ def parse_small_data(infile, outfile1, outfile2):
     json.dump(subfile1, sub_result_1)
     json.dump(subfile2, sub_result_2)
 
+
+def parse_integrate_data(infile, outfile):
+    file = open(infile)
+    result = open(outfile, 'w', encoding='utf-8')
+    node_dict = {}
+    for i in range(4039):
+        node_dict[str(i)] = {
+            'degree': 0,
+            'inlink': [],
+        }
+    line = file.readline()
+    while line:
+        arr = line.split()
+        node1 = arr[0]
+        node2 = arr[1]
+
+        node_dict[node1]['degree'] += 1
+        node_dict[node1]['inlink'].append(node2)
+        node_dict[node2]['degree'] += 1
+        node_dict[node2]['inlink'].append(node1)
+
+        line = file.readline()
+
+    json.dump(node_dict, result)
+
+
 if __name__ == '__main__':
     parse_small_data("facebook_combined.txt", "facebook_combined_sub1.json", "facebook_combined_sub2.json")
