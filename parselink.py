2cbfc4abd45f8497464f60da6ba900b54d802d6a
alienware m15 <1033718596@qq.com>
2019-06-04 12:58:38 +0800

    spilted file

--- parselink.py
+++ parselink.py
@@ -1,9 +1,10 @@
 import json
 
 
-def parse_small_data():
-    file = open("facebook_combined.txt")
-    result = open("facebook_combined.json", 'w', encoding='utf-8')
+def parse_small_data(infile, outfile1, outfile2):
+    file = open(infile)
+    sub_result_1 = open(outfile1, 'w', encoding='utf-8')
+    sub_result_2 = open(outfile2, 'w', encoding='utf-8')
     node_dict = {}
     for i in range(4039):
         node_dict[str(i)] = {
@@ -22,7 +23,11 @@ def parse_small_data():
         node_dict[node2]['inlink'].append(node1)
 
         line = file.readline()
-    json.dump(node_dict, result)
+
+    subfile1 = {str(i): node_dict[str(i)] for i in range(2020)}
+    subfile2 = {str(i): node_dict[str(i)] for i in range(2020, 4039)}
+    json.dump(subfile1, sub_result_1)
+    json.dump(subfile2, sub_result_2)
 
 if __name__ == '__main__':
-    parse_small_data()
+    parse_small_data("facebook_combined.txt", "facebook_combined_sub1.json", "facebook_combined_sub2.json")
